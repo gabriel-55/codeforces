52f80024c0ab5b6d3a17c4deaa8f701a115bf1c5
Gabriel <renokoku@gmail.com>
2022-10-03 06:20:31 +1300

    commit

---
+++ cf824/b.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+ 
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+ 
+void solve() {
+   ll n;
+   cin >> n;
+   ll mn = inf;
+   vector<ll> a(n);
+   rep(i,n) {
+      cin >> a[i];
+      chmin(mn, a[i]);
+   }
+   mn = mn*2-1;
+
+   ll ans = 0;
+   rep(i,n) {
+      if(mn+1 > a[i]) continue;
+      ll x = (a[i] + mn - 1) / mn;
+      ans += x-1;
+   }
+   cout << ans << '\n';
+}
+ 
+int main() {
+   ll t = 1;
+   cin >> t;
+   while(t--) solve();
+   return 0;
+}
